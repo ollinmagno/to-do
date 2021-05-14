ac9557949db0b92a1d9285361618c2fdb9d3752b
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-05-13 21:20:39 -0300

    método post

--- src/main/java/br/com/bwn/controlador/CriarTarefa.java
+++ src/main/java/br/com/bwn/controlador/CriarTarefa.java
@@ -2,7 +2,11 @@ package br.com.bwn.controlador;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +23,15 @@ public class CriarTarefa {
        this.repositorio = tarefaRepositorio;
    }
    
+   @PostMapping
+   public Tarefa criarTarefa(@RequestBody Tarefa tarefa){
+      return repositorio.save(tarefa);
+   }
+   
    @GetMapping
    public List<Tarefa> findAll(){
       return repositorio.findAll();
    }
+   
    
 }
