534518c9b8f76178418d341916e474945768b872
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-05-13 21:42:35 -0300

    procurar tarefa por id

--- src/main/java/br/com/bwn/controlador/CriarTarefa.java
+++ src/main/java/br/com/bwn/controlador/CriarTarefa.java
@@ -1,11 +1,14 @@
 package br.com.bwn.controlador;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,24 +17,36 @@ import br.com.bwn.modelo.Tarefa;
 import br.com.bwn.repositorio.ITarefaRepositorio;
 
 @RestController
-@RequestMapping({"/tarefas"})
+@RequestMapping({ "/tarefas" })
 public class CriarTarefa {
 
-   private ITarefaRepositorio repositorio;
+	private ITarefaRepositorio repositorio;
 
-   CriarTarefa(ITarefaRepositorio tarefaRepositorio) {
-       this.repositorio = tarefaRepositorio;
-   }
-   
-   @PostMapping
-   public Tarefa criarTarefa(@RequestBody Tarefa tarefa){
-      return repositorio.save(tarefa);
-   }
-   
-   @GetMapping
-   public List<Tarefa> findAll(){
-      return repositorio.findAll();
-   }
-   
-   
+	CriarTarefa(ITarefaRepositorio tarefaRepositorio) {
+		this.repositorio = tarefaRepositorio;
+	}
+
+	@PostMapping
+	public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
+		return repositorio.save(tarefa);
+	}
+
+	@GetMapping(path = { "/{id}" })
+	public ResponseEntity<Tarefa> encontrarPorID(@PathVariable long id) {
+		Optional<Tarefa> tarefas = repositorio.findById(id);
+		
+		if (tarefas.isPresent()) {
+			return new ResponseEntity<Tarefa>(tarefas.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Tarefa>(new Tarefa(), HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	
+	
+	@GetMapping
+	public List<Tarefa> findAll() {
+		return repositorio.findAll();
+	}
+
 }
