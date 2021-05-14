22c2aa7f7737fb76826069b9eb6ea8098632162f
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-05-13 21:00:37 -0300

    modelagem da classe Tarefa

---
+++ src/main/java/br/com/bwn/modelo/Tarefa.java
@@ -0,0 +1,72 @@
+package br.com.bwn.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tarefa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+	private String descricao;
+	private boolean ativo;
+	private boolean realizado;
+
+	Tarefa() {
+
+	}
+
+	public Tarefa(Long id, String nome, String descricao, boolean ativo, boolean realizado) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.ativo = ativo;
+		this.realizado = realizado;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public boolean isRealizado() {
+		return realizado;
+	}
+
+	public void setRealizado(boolean realizado) {
+		this.realizado = realizado;
+	}
+
+}
