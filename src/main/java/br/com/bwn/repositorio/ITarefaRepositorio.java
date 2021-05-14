88291175a53f91eb784f77296672620b0726d9a0
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-05-14 10:03:00 -0300

    interface @Repository adicionada

--- src/main/java/br/com/bwn/repositorio/ITarefaRepositorio.java
+++ src/main/java/br/com/bwn/repositorio/ITarefaRepositorio.java
@@ -1,8 +1,11 @@
 package br.com.bwn.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import br.com.bwn.modelo.Tarefa;
 
+@Repository
 public interface ITarefaRepositorio extends JpaRepository<Tarefa, Long> {
 
 }
